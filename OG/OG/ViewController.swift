c7e1d23b394a1cdafa5a6048064a287ea77f6efd
Gun woo Lim <lkwyoyo@gmail.com>
2019-09-02 13:53:21 +0900

    push all

---
+++ OG/OG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OG
+//
+//  Created by 임건우 on 02/09/2019.
+//  Copyright © 2019 lims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
